84ab9fd0d0d0b57ed0c80df630376b04d6501a05
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-07-26 14:59:29 -0600

    Up

--- models/purchase_order.py
+++ models/purchase_order.py
@@ -1,5 +1,4 @@
 from odoo import models, fields, api
-from datetime import datetime
 
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
@@ -11,6 +10,7 @@ class PurchaseOrder(models.Model):
         record = super(PurchaseOrder, self).create(vals)
         if 'is_authorized' in vals:
             self._log_authorization_change(record, vals['is_authorized'])
+        self._update_sequence_prefix(record)
         return record
 
     def write(self, vals):
@@ -18,6 +18,8 @@ class PurchaseOrder(models.Model):
         if 'is_authorized' in vals:
             for record in self:
                 self._log_authorization_change(record, vals['is_authorized'])
+        for record in self:
+            self._update_sequence_prefix(record)
         return res
 
     def _log_authorization_change(self, record, new_value):
@@ -29,3 +31,17 @@ class PurchaseOrder(models.Model):
             timestamp
         )
         record.message_post(body=message)
+
+    def _update_sequence_prefix(self, record):
+        if not record.is_authorized:
+            record.name = self.env['ir.sequence'].next_by_code('purchase.order.draft') or 'RDM'
+        elif record.state == 'purchase':
+            record.name = self.env['ir.sequence'].next_by_code('purchase.order.confirmed') or 'OC'
+        elif record.state == 'done':
+            record.name = self.env['ir.sequence'].next_by_code('purchase.order.done') or 'PC'
+
+    @api.multi
+    def action_set_done(self):
+        for order in self:
+            order.state = 'done'
+            order._update_sequence_prefix()
