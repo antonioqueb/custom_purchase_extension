afcce9f6bc631fd8b3642c5d3a4a90c3062fed75
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-07-26 13:14:01 -0600

    Up

--- models.py
+++ models.py
@@ -1,6 +1,17 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
 
     is_authorized = fields.Boolean(string='Autorizado', default=False)
+
+    @api.multi
+    def write(self, vals):
+        if 'is_authorized' in vals:
+            for record in self:
+                old_value = record.is_authorized
+                new_value = vals['is_authorized']
+                if old_value != new_value:
+                    message = "Authorization status changed from %s to %s" % (old_value, new_value)
+                    record.message_post(body=message)
+        return super(PurchaseOrder, self).write(vals)
