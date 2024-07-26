9df638b092242179e86a06851ebff13b28548624
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-07-26 13:06:25 -0600

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Custom Purchase Extension',
+    'version': '1.1',
+    'summary': 'Extension to add is_authorized field to Purchase Order',
+    'description': 'This module adds a custom field to indicate if the purchase order is authorized.',
+    'author': 'ALPHAQUEB CONSULTING S.A.S.',
+    'depends': ['purchase'],
+    'data': [],
+    'installable': True,
+    'application': False,
+}
---
+++ models.py
@@ -0,0 +1,6 @@
+from odoo import models, fields
+
+class PurchaseOrder(models.Model):
+    _inherit = 'purchase.order'
+
+    is_authorized = fields.Boolean(string='Autorizado', default=False)
