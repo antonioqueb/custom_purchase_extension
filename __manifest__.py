c478a842d0b5b2e1a941fe9fc4a26973e6a7df60
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-08-26 19:29:53 -0600

    Up

--- __manifest__.py
+++ __manifest__.py
@@ -1,11 +1,13 @@
 {
     'name': 'Custom Purchase Extension',
-    'version': '1.3',
+    'version': '1.4',
     'summary': 'Extension to add is_authorized field to Purchase Order',
     'description': 'This module adds a custom field to indicate if the purchase order is authorized.',
     'author': 'ALPHAQUEB CONSULTING S.A.S.',
     'depends': ['purchase'],
-    'data': [],
+    'data': [
+    'views/purchase_order_view_inherit.xml',
+    ],
     'installable': True,
     'application': False,
 }
