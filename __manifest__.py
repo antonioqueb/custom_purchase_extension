a63d3311e4c3e586b1ce34614d64551c3d031639
Antonio Queb <antonioqueb@alphaqueb.online>
2024-11-13 13:14:52 -0600

    Up

--- __manifest__.py
+++ __manifest__.py
@@ -4,7 +4,7 @@
     'summary': 'Extension to add is_authorized field to Purchase Order',
     'description': 'This module adds a custom field to indicate if the purchase order is authorized.',
     'author': 'ALPHAQUEB CONSULTING S.A.S.',
-    'depends': ['purchase'],
+    'depends': ['base', 'purchase'],
     'data': [
     'views/purchase_order_view_inherit.xml',
     ],
